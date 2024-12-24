ade58f87714f30766e205cc1b5226b4755176818
Clayton Cheng <clay578@gmail.com>
2024-12-24 11:21:48 -0800

    add home link

--- app/src/components/layout/Navbar.tsx
+++ app/src/components/layout/Navbar.tsx
@@ -8,7 +8,11 @@ const Navbar = () => {
       <Link className="nav-title nav-link" to="/">Meal Prep</Link>
 
       <ul className="nav-ul">
-
+        
+        {/* Links */}
+        <li className="nav-li">
+          <Link className="nav-link" to="/">Home</Link>
+        </li>
         <li className="nav-li">
           <Link className="nav-link" to="/recipes">Recipes</Link>
         </li>
@@ -18,7 +22,6 @@ const Navbar = () => {
         <li className="nav-li">
           <Link className="nav-link" to="/days">Days</Link>
         </li>
-
       </ul>
 
     </nav>
